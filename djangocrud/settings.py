2e2327cad7e2ea79d0698813db425598cc2f6588
slartdesigns <slart700@gmail.com>
2024-10-22 20:40:55 -0400

    fix storages static files

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -156,6 +156,11 @@ if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
+    STORAGES = {
+        'staticfiles': {
+        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+        },
+    }
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
 
 # Default primary key field type
