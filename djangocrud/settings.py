1ddd089a1c9d3c4d65d830144b3dfcb6e2cd04f3
slartdesigns <slart700@gmail.com>
2024-10-23 08:53:24 -0400

    fixing statics

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -151,17 +151,13 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
-# if not DEBUG:
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STORAGES = {
-    'staticfiles': {
-    'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
-    },
-}
-    # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-    # STATICFILES_DIRS = (
-    #     os.path.join(BASE_DIR, 'static'),
-    # )
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STORAGES = {
+        'staticfiles': {
+        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+        },
+    }
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -17,16 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from django.conf import settings
-from django.conf.urls.static import static
-
 urlpatterns = [
     path('__reload__/', include('django_browser_reload.urls')),
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('auth/', include('dashboard.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
