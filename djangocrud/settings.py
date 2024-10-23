e0a8fa13b46ff5dc8e1c699f4e58273b7099147e
slartdesigns <slart700@gmail.com>
2024-10-23 13:51:08 -0400

    reverse changes

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -27,11 +27,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['kdisell.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
-# RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-# if RENDER_EXTERNAL_HOSTNAME:
-#     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd" 
 
